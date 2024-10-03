287c7c024fb096534b6958ce6cbf359fcb1e8206
SebastianHernandez69 <sh3641046@gmail.com>
2024-10-02 22:13:25 -0600

    Firts commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 3001;
+
+//
+app.get("/filter",(req, res) => {
+    const results = [];
+    const { column, value, page = 1, pageSize = 10 } = req.query;
+    const startI = (page - 1) * pageSize;
+    const endI = page * pageSize;
+
+    try {
+        
+        fs.createReadStream('src/data/ejemplo.csv')
+        .pipe(csv())
+        .on('data', (data) => {
+            if (data[column] === value) {
+                results.push(data);
+            }
+        })
+        .on('end', () => {
+            const paginatedResults = results.slice(startI, endI);
+            res.json({
+                page: parseInt(page),
+                pageSize: parseInt(pageSize),
+                total: results.length,
+                resultados: paginatedResults
+            });
+        })
+        .on('error', (error) => {
+            console.error('Error al leer archivo CSV:', error);
+            res.status(500).json({ message: 'Error al leer el archivo CSV' });
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error general' });
+    }
+
+    
+
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
