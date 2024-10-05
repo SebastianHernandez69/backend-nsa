7368045e9a5d2489ea6241d17f55d7b011b6ac11
SebastianHernandez69 <sh3641046@gmail.com>
2024-10-05 01:58:52 -0600

    endpoints asteroids and comets

--- app.js
+++ app.js
@@ -1,48 +1,85 @@
 const express = require('express');
 const fs = require('fs');
 const csv = require('csv-parser');
+const { default: axios } = require('axios');
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3001;
 
-// http://localhost:3001/filter?column=age&value=25&page=1&pageSize=10
-app.get("/filter",(req, res) => {
+// ASTEROIDS
+app.get("/asteroids",(req, res) => {
+
     const results = [];
-    const { column, value, page = 1, pageSize = 10 } = req.query;
-    const startI = (page - 1) * pageSize;
-    const endI = page * pageSize;
+    const { page = 1, limit = 10 } = req.query;
+    const columns = ['full_name', 'a', 'e', 'i', 'per_y', 'diameter']
 
     try {
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
         
-        fs.createReadStream('src/data/ejemplo.csv')
-        .pipe(csv())
-        .on('data', (data) => {
-            if (data[column] === value) {
-                results.push(data);
-            }
-        })
-        .on('end', () => {
-            const paginatedResults = results.slice(startI, endI);
-            res.json({
-                page: parseInt(page),
-                pageSize: parseInt(pageSize),
-                total: results.length,
-                resultados: paginatedResults
+        fs.createReadStream('src/data/asteroids.csv')
+            .pipe(csv())
+            .on('data', (asteroids) => {
+
+                const filteredAsteroids = {};
+                columns.forEach(column => {
+                    if(asteroids[column] !== undefined){
+                        filteredAsteroids[column] = asteroids[column];
+                    }
+                });
+                results.push(filteredAsteroids);
+            })
+            .on('end', () => {
+                const paginatedAsteroids = results.sort((asteroid1, asteroid2) => parseFloat(asteroid2.a) > parseFloat(asteroid1.a) ? -1 : 1)
+                .slice(startIndex, endIndex);
+
+                res.json(paginatedAsteroids);
+            })
+            .on('error', (err) => {
+                console.log(err);
+                res.status(500).json({ message: 'Error' });
             });
-        })
-        .on('error', (error) => {
-            console.error('Error al leer archivo CSV:', error);
-            res.status(500).json({ message: 'Error al leer el archivo CSV' });
-        });
         
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error general' });
+        res.status(500).json({ message: 'Error' });
     }
 
-    
+})
 
+// COMETS
+app.get("/comets", async (req, res) => {
+    
+    const { page = 1, limit = 10 } = req.query;
+
+    try {
+
+        startIndex = (page - 1) * limit;
+        endIndex = page * limit;
+
+        const response = await axios.get('https://data.nasa.gov/resource/b67r-rgxc.json');
+        let comets = response.data;
+
+        comets = comets.map(comet => ({
+            e: comet.e,
+            i_deg: comet.i_deg,
+            q_au_1: comet.q_au_1,
+            q_au_2: comet.q_au_2,
+            p_yr: comet.p_yr,
+            moid_au: comet.moid_au,
+            object: comet.object
+        }))
+        .sort((comet1, comet2) => comet2.e > comet1.e ? -1 : 1)
+        .slice(startIndex, endIndex);
+
+        res.json(comets);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error' });
+    }
 
 })
 
